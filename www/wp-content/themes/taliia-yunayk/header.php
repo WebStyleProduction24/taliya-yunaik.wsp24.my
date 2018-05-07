d3e777d8e08ad38c9efa4208a1c1ac725f9dea9b
Jack36vrn <Jack36@list.ru>
2018-05-07 19:40:25 +0300

    v. 0.0.3
    Подготовлена тема к работе

---
+++ www/wp-content/themes/taliia-yunayk/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<title><?php echo wp_get_document_title(); ?></title>
+		<?php wp_head(); ?>
+</head>
+
+<body>
+
+	<header>
+		
+	</header>
