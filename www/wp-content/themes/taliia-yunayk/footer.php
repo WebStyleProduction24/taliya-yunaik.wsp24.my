941f5fdde52ffa2ebde9b8f91f32853e7ce2906c
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2018-05-15 20:35:39 +0300

    v.2.0
    Готова главная страница и Footer

--- www/wp-content/themes/taliia-yunayk/footer.php
+++ www/wp-content/themes/taliia-yunayk/footer.php
@@ -1,8 +1,46 @@
         <footer>
-            
-        </footer>
+        	<div>
+        		<div>		
+        			<div>
+        				<h5>Информация</h5>
 
+        				<?php wp_nav_menu( array(
+        					'container'       => 'div', 
+        					'container_class' => 'f-block-cnt', 
+        					'menu_class'      => 'list-small'
+
+        				) ); ?>
+
+        			</div>
+        		</div>
+        		<div>
+        			<div>
+        				<div>
+        					<h5>контакты</h5>
+        					<div class="f-block-cnt">
+        						<div class="f-contact"><i class="icon-home"></i><a href="#">????????</a></div>
+        						<div class="f-contact"><i class="icon-phone"></i><a href="tel:">????????</a></div>
+        						<div class="f-contact"><i class="icon-envelop"></i><a href="mailto:">????????</a></div>
+
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        		<div>
+        			<div>
+        				<div class="f-soc">
+        					<a href="#" target="_blank"><i class="icon-vk"></i></a>
+        					<a href="#" target="_blank"><i class="icon-instagram"></i></a>
+        				</div>	
+        			</div>
+        		</div>
+        	</div>
+        	<div class="copiryte"><a href="/">&copy;2018 Taliia Yunaik<a>. Все права защищены.<br><a href="https://wsp24.ru/" target="_blank">Web Style production 24</a></div>
+
+        </footer>
+        
         <?php wp_footer(); ?>
 
-    </body>
-</html>
+
+      </body>
+      </html>
