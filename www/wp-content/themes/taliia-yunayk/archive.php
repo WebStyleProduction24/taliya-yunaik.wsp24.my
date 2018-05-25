16ad47dc8060ef2ae39c255d91ed75faa9540524
Jack36vrn <Jack36@list.ru>
2018-05-25 17:28:23 +0300

    v.2.5
    1. Установлен плагин contact-form
    2. Доработан archive.php, для отображения страниц категорий
    3. Обновлен old.css
    4. Доработан sidebar.php для отображения страниц с фотогралереями по категориям
    5. Обновлен style.css
    6. Начата работа над контактными формами

--- www/wp-content/themes/taliia-yunayk/archive.php
+++ www/wp-content/themes/taliia-yunayk/archive.php
@@ -1,1 +1,21 @@
-test
+<?php get_header(); ?>
+<div id="path">
+	    <div class="cnt oh">
+		    <div class="path-title">﻿</div>
+			
+		</div>
+	</div>
+<main class="ind-page">
+	<div class="post">
+      <div class="sidebar-content"> 
+        <?php
+            if (have_posts()): while( have_posts()) : the_post();
+                get_template_part( 'single', 'post' );
+            endwhile; endif;
+        ?>
+			</div>
+      <?php get_sidebar(); ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
